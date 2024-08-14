050db757085ac3dd600feb7288cdb4acbb0c150a
kumar jyothiranjan <jyothiranjan25@gmail.com>
2024-08-14 09:15:51 +0530

    assignment

--- ScoreCard.java
+++ ScoreCard.java
@@ -1,8 +1,31 @@
-public class ScoreCard{
-// Complete Your Code Here
+package If_Else_Assignemnts;
+
+public class ScoreCard {
+  // Complete Your Code Here
   // Use Scanner
   // Use && Operator
   // Use Operators >= && <=
-  // ID Name Score 
+  // ID Name Score
   // Calculate Grade and Print Grade
+
+  public static void main(String[] args) {
+    java.util.Scanner sc = new java.util.Scanner(System.in);
+    System.out.println("Enter ID: ");
+    int id = sc.nextInt();
+    System.out.println("Enter Name: ");
+    String name = sc.next();
+    System.out.println("Enter Score: ");
+    double score = sc.nextDouble();
+    if (score >= 90 && score <= 100) {
+      System.out.println("The Grade for " + name + " is A");
+    } else if (score >= 80 && score < 90) {
+      System.out.println("The Grade for " + name + " is B");
+    } else if (score >= 70 && score < 80) {
+      System.out.println("The Grade for " + name + " is C");
+    } else if (score >= 60 && score < 70) {
+      System.out.println("The Grade for " + name + " is D");
+    } else {
+      System.out.println("The Grade for " + name + " is F");
+    }
+  }
 }
