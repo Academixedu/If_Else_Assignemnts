5348d1e7f3a6bb70fe9351d10cd8a31a64d8649c
kumar jyothiranjan <jyothiranjan25@gmail.com>
2024-08-14 09:21:35 +0530

    assignment

--- DrivingLicense.java
+++ DrivingLicense.java
@@ -1,3 +1,14 @@
-public class DrivingLicense{
-// Complete Your Code Here
+package If_Else_Assignemnts;
+public class DrivingLicense {
+    public static void main(String[] args) {
+        int age = 17;
+        if (age >= 18 && age <= 60) {
+            System.out.println("You are eligible for Driving License");
+        } else if (age < 18) {
+            System.out.println("You are not eligible for Driving License");
+        } else {
+            System.out.println("You are not eligible for Driving License as you are above 60");
+        }
+    }
+
 }
