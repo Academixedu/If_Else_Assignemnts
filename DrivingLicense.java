bcc52cb557aa286f4316b7f6392cdc083d87391f
karthhiik <karthikthatikonda18@gmail.com>
2024-08-14 09:17:56 +0530

    Update DrivingLicense.java

--- DrivingLicense.java
+++ DrivingLicense.java
@@ -1,3 +1,14 @@
+import java.util.*;
 public class DrivingLicense{
 // Complete Your Code Here
+  public static void main(String[] args){
+    int n;
+    Scanner sc = new Scanner(System.in);
+    n=sc.nxtInt();
+    if(age>=18)
+    System.out.println(x:He is Elegible);
+    else
+     System.out.println(x:"He is not Elegible");
+  }
+
 }
