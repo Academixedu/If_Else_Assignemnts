b21c4096913310f145b7666f8df6244960536b58
Talari-Lavanya <lavanya62811@gmail.com>
2024-08-14 11:23:10 +0530

    from Lavanya

--- ScoreCard.java
+++ ScoreCard.java
@@ -1,8 +1,47 @@
+import java.util.Scanner;
+
 public class ScoreCard{
-// Complete Your Code Here
+  
+  // Complete Your Code Here
   // Use Scanner
   // Use && Operator
   // Use Operators >= && <=
   // ID Name Score 
   // Calculate Grade and Print Grade
+  public static void main(String[] args){
+    int id;
+    String name;
+    double Score;
+    ScoreCard SC=new ScoreCard();
+    Scanner in=new Scanner(System.in);
+    System.out.println("enter id");
+    id=in.nextInt();
+    System.out.println("enter name");
+    name=in.nextLine();
+    in.nextLine();
+    System.out.println("enter Score");
+    Score=in.nextDouble();
+     
+       if( Score>=90 && Score<=100){
+      System.out.println("A grade");
+     }
+     else if(Score>=80 && Score<=90){
+      System.out.println("B grade");
+     }
+     else if(Score>=75 && Score<=80){
+      System.out.println("C grade");
+     }
+     else if(Score<=75){
+      System.out.println("fail");
+     }
+     
+  }
 }
+
+
+
+
+
+
+  
+
